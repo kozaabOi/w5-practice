a385f1ee6a3716ec0f5be1fa3d98e8720deebb17
Mark Bodis <75426536+kozaabOi@users.noreply.github.com>
2023-10-26 12:03:44 +0200

    CSS

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ console.log('loaded')
 const rootElement = document.querySelector("#root")
 console.log(rootElement)
 
+const fetchUrl = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
 const countryComponent = (country) => `
     <div class="country">
         <h2>country name: ${country.name.common}</h2>
@@ -10,63 +16,23 @@ const countryComponent = (country) => `
     </div>
 `
 
-async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
-    const response = await fetch("https://restcountries.com/v3.1/all") // megvárjuk a fetch válaszát (promise helyett)
-    const data = await response.json() // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
-    console.log(data)
-
+async function init() { 
+    const data = await fetchUrl("https://restcountries.com/v3.1/all")
+    
     data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
 
+    /*const countryElement = document.querySelector("div.country")
+    console.log(countryElement)
+    countryElement.addEventListener("click", () => {
+        countryElement.classList.toggle("clicked")
+    })*/
+
+    const countryElements = document.querySelectorAll("div.country")
+    countryElements.forEach((countryElement) => {
+        countryElement.addEventListener("click", () => {
+            countryElement.classList.toggle("clicked")
+        })
+    })
 }
 
 init()
-
-
-/* rootElement.innerHTML = `
-    <h1>hello world</h1>
-    <div>
-        <p>lorem ipsum</p>
-        <h2>subtitle</h2>
-        <button>click me!</button>
-    </div>
-`
-
-const array = ["kismacska", "kutyus", "zebra", "kecske"] */
-
-/* const jsonLikeArray = [
-    {
-        countryName: "Hungary",
-        population: 8900000
-    },
-    {
-        countryName: "England",
-        population: 18000000
-    },
-    {
-        countryName: "USA",
-        population: 230000000
-    }
-]
-
-for (let i = 0; i < jsonLikeArray.length; i++) {
-    rootElement.innerHTML += `
-        <h3>name: ${jsonLikeArray[i].countryName}</h3>
-        <h4>population: ${jsonLikeArray[i].population}</h4>
-    `
-} */
-
-/*fetch("https://restcountries.com/v3.1/all") // elküldjük adatért a JS-t, visszatér valamikor adattal
-    .then((res) => res.json()) // megjött az adat, de ki kell csomagolni, visszatér valamikor a kicsomagolt adattal
-    .then((data) => { // megjött a kicsomagolt adat, innentől használhatjuk
-        console.log(data)
-
-        for (let i = 0; i <data.length; i++) {
-            console.log(data[i].name.common)
-            console.log(data[i].population)
-
-            rootElement.innerHTML += `
-                <h3>country name: ${data[i].name.common}</h3>
-                <h4>country population: ${data[i].population}</h4>
-            `
-        }
-    }) */
